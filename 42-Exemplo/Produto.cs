3eff789eb556a651c610dceab50a534f97a2306b
Rafael Dos Santos <raphael_979@hotmail.com>
2020-03-17 15:46:16 -0400

    Orientação objetos com Classe Produto e metodos add remover

---
+++ 42-Exemplo/Produto.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Globalization;
+
+
+namespace _42_Exemplo
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+
+        }
+
+        public void AdcionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+
+        }
+
+
+        public override string ToString()
+        {//Aqui o produto vai ser transformado na forma de String
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + " ,"
+                + Quantidade
+                + " Unidades, total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 42-Exemplo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace _42_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa para ler os dados de  um produto em estoque(nome,preco e quantidade no estoque).m seguida
+            •Mostras os dados do produto(nome,preco,quantidade no estoque,valor total no estoque.
+            •Realizar uma entrada no estoque e mostrar novamente os dados do prdouto
+            •Vamos ultilizar o toString,converte objeto para String
+            •Realizar uma saída no estoque e mostrar novamente os dados do produto */
+
+            Produto p = new Produto();  //Aqui instanciando Produto com variavel p
+            Console.WriteLine("Entre com os dados do Produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no Estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto:" + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdcionarProdutos(qte);
+            Console.WriteLine("Dados atualizados:" + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto:" + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser Removido ao estoque: ");
+             qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine("Dados atualizados:" + p);
+
+
+
+
+
+        }
+    }
+}
